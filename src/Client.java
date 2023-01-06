73f022532600cf91a63d215a4418fb51565ccca2
GeorgePopaA <poplike14@yahoo.com>
2023-01-06 14:17:18 +0200

    Implementation of Static method for abstract factory class. third example of implementation for Factory.

---
+++ src/Client.java
@@ -0,0 +1,13 @@
+public class Client {
+    public static void main(String[] args) {
+        Shape shape1 = ShapeFactory.getShape("CIRCLE");
+        shape1.draw();
+
+        Shape shape2 = ShapeFactory.getShape("SQUARE");
+        shape2.draw();
+
+        Shape shape3 = ShapeFactory.getShape("RECTANGLE");
+        shape3.draw();
+
+    }
+}
